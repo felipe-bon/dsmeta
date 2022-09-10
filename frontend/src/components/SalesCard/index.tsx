f12fef17770ea8f5ad68d8b2ae540f2f52c38520
Felipe Bonetti <felipebonetti2001@hotmail.com>
2022-09-10 09:01:28 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -17,6 +17,10 @@ function SalesCard() {
   
     const [sales, setSales] = useState<Sale[]>([]);
 
+    type Props = {
+        saleId: number;
+    }
+
     useEffect(() => {
 
         const dmin = minDate.toISOString().slice(0,10);
@@ -76,7 +80,7 @@ function SalesCard() {
                                                     <td>{sale.amount.toFixed(2)}</td>
                                                     <td>
                                                         <div className="dsmeta-red-btn-container">
-                                                            <NotificationButton/>
+                                                            <NotificationButton saleId={sale.id}/>
                                                         </div>
                                                     </td>
                                                 </tr>
